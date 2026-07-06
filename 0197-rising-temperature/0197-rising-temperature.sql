746327a3bf0713b9b938f5c6e560ab4aa37a85fd
Deepansh Kushwaha <deepanshthakur866@gmail.com>
2026-07-06 22:30:12 +0530

    Time: 526 ms (35.99%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,5 +3,6 @@ select w1.id
 from Weather w1
 inner join Weather w2
 -- where datediff(w1.recordDate, w2.recordDate) = 1
-on datediff(w1.recordDate, w2.recordDate) = 1
+-- on datediff(w1.recordDate, w2.recordDate) = 1
+where SUBDATE(w1.recordDate, 1) = w2.recordDate
 and w1.temperature > w2.temperature;
